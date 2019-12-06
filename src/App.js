94904fd0abdc8f354eac4fc4fbc1c8996e055926
jake-schroeder-isx <jake.schroeder@isophex.com>
2019-12-05 23:57:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,184 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Form, Field, useField, FieldArray } from "formik";
+import * as yup from "yup";
+import {
+  TextField,
+  Button,
+  Checkbox,
+  Radio,
+  FormControlLabel,
+  Select,
+  MenuItem
+} from "@material-ui/core";
+
+//custom form field
+const MyRadio = ({ label, ...props }) => {
+  const [field] = useField(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
+
+//custom validation
+const MyTextField = ({ placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+
+  const errorText = meta.error && meta.touched ? meta.error : "";
+  return (
+    <TextField
+      placeholder={placeholder}
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+    />
+  );
+};
+
+const validationSchema = yup.object({
+  firstName: yup
+    .string()
+    .required()
+    .max(10),
+  pets: yup.array().of(
+    yup.object({
+      name: yup.string().required()
+    })
+  )
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          isTall: "",
+          cookies: [],
+          yogurt: "",
+          pets: [{ type: "cat", name: "jarvis", id: Math.random() }]
+        }}
+        // validate={values => {
+        //   const errors = {};
+
+        //   if (values.firstName.includes("bob")) {
+        //     errors.firstName = "no bob";
+        //   }
+
+        //   return errors;
+        // }}
+        validationSchema={validationSchema}
+        onSubmit={(data, { setSubmitting }) => {
+          setSubmitting(true);
+          //make async call
+          console.log(data);
+          setSubmitting(false);
+        }}
+      >
+        {({ values, errors, isSubmitting }) => (
+          <Form>
+            <MyTextField
+              placeholder="first name"
+              name="firstName"
+              type="input"
+            />
+            <div>
+              <Field
+                placeholder="last name"
+                name="lastName"
+                type="input"
+                as={TextField}
+              />
+            </div>
+            <div>
+              <Field name="isTall" type="checkbox" as={Checkbox} />
+            </div>
+            <div>Cookies:</div>
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="chocolate chip"
+              as={Checkbox}
+            />
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="snickerdoodle"
+              as={Checkbox}
+            />
+            <Field
+              name="cookies"
+              type="checkbox"
+              value="oatmeal"
+              as={Checkbox}
+            />
+            <div>Yogurt:</div>
+
+            <MyRadio name="yogurt" type="radio" value="peach" label="peach" />
+            <MyRadio
+              name="yogurt"
+              type="radio"
+              value="blueberry"
+              label="blueberry"
+            />
+            <MyRadio
+              name="yogurt"
+              type="radio"
+              value="vanilla"
+              label="vanilla"
+            />
+
+            <FieldArray name="pets">
+              {arrayHelpers => (
+                <div>
+                  <Button
+                    onClick={() =>
+                      arrayHelpers.push({
+                        type: "frog",
+                        name: "",
+                        id: "" + Math.random()
+                      })
+                    }
+                  >
+                    add pet
+                  </Button>
+                  {values.pets.map((pet, index) => {
+                    return (
+                      <div
+                        style={{ display: "flex", alignItems: "flex-end" }}
+                        key={pet.id}
+                      >
+                        <MyTextField
+                          placeholder="pet name"
+                          name={`pets.${index}.name`}
+                        />
+                        <Field
+                          name={`pets.${index}.type`}
+                          type="select"
+                          as={Select}
+                        >
+                          <MenuItem value="cat">cat</MenuItem>
+                          <MenuItem value="dog">dog</MenuItem>
+                          <MenuItem value="frog">frog</MenuItem>
+                        </Field>
+                        <Button onClick={() => arrayHelpers.remove(index)}>
+                          x
+                        </Button>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </FieldArray>
+
+            <div>
+              <Button disabled={isSubmitting} type="submit">
+                Submit
+              </Button>
+            </div>
+            <pre>{JSON.stringify(errors, null, 2)}</pre>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
